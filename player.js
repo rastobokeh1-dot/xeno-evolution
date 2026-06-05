3462288c505e0d4b9e26dca0122fbdd1910e43a8
rastobokeh1-dot <rastobokeh1@gmail.com>
2026-06-05 20:48:51 +0200

    Update player.js

--- player.js
+++ player.js
@@ -1,20 +1,24 @@
 window.Player = {
-    mesh: null, core: null, aura: null,
+    mesh: null, core: null, aura: null, coreMat: null,
     posX: 0, posY: 0, velocityX: 0, velocityY: 0, speed: 0.15,
-    trail: [],
-    appendages: [], // 🦑 Nové: Fyzické končatiny/tykadlá
-    baseScale: 1.0,
+    trail: [], appendages: [], baseScale: 1.0,
 
     init() {
         this.mesh = new THREE.Group();
 
-        // Jadro bunky
-        const coreGeom = new THREE.IcosahedronGeometry(1, 2);
-        const coreMat = new THREE.MeshPhongMaterial({ color: 0x0ea5e9, emissive: 0x0284c7, shininess: 100 });
-        this.core = new THREE.Mesh(coreGeom, coreMat);
+        // JADRO BUNKY - POUŽITIE NOVÉHO SHADERU
+        const coreGeom = new THREE.IcosahedronGeometry(1, 32);
+        this.coreMat = new THREE.ShaderMaterial({
+            uniforms: THREE.UniformsUtils.clone(window.BioShaders.liquidCell.uniforms),
+            vertexShader: window.BioShaders.liquidCell.vertexShader,
+            fragmentShader: window.BioShaders.liquidCell.fragmentShader,
+            transparent: true,
+            side: THREE.DoubleSide
+        });
+        this.core = new THREE.Mesh(coreGeom, this.coreMat);
         this.mesh.add(this.core);
 
-        // Žiariaca aura
+        // AURA BUNKY
         const auraGeom = new THREE.SphereGeometry(1.6, 16, 16);
         const auraMat = new THREE.MeshBasicMaterial({
             color: 0x38bdf8, transparent: true, opacity: 0.3, blending: THREE.AdditiveBlending
@@ -24,6 +28,7 @@ window.Player = {
 
         scene.add(this.mesh);
 
+        // OVLÁDANIE
         const moveHandler = (clientX, clientY) => {
             const dirX = clientX - window.innerWidth / 2;
             const dirY = -(clientY - window.innerHeight / 2);
@@ -40,45 +45,31 @@ window.Player = {
         window.addEventListener("mouseup", () => { this.velocityX = 0; this.velocityY = 0; });
     },
 
-    // 🦑 PROCEDURÁLNA MUTÁCIA: Rast tykadiel
     evolveShape() {
         if (!window.Entities) return;
-        
-        // Vypočítame, koľko tykadiel by mala bunka mať (1 tykadlo na každých 8 zjedených biomás)
         const targetAppendages = Math.floor(window.Entities.biomassCount / 8);
         
-        // Ak máme menej tykadiel, ako si zaslúžime, vygenerujeme nové!
         if (this.appendages.length < targetAppendages && this.appendages.length < 12) {
-            console.log("🧬 Evolučný skok: Bunka mutuje a rastie tykadlo!");
-            
             const geom = new THREE.ConeGeometry(0.15, 1.2, 5);
-            geom.translate(0, 0.6, 0); // Posunieme, aby sa otáčali zospodu, nie zo stredu
-            
+            geom.translate(0, 0.6, 0);
             const mat = new THREE.MeshPhongMaterial({ color: 0x0ea5e9, emissive: 0x0284c7 });
             const spike = new THREE.Mesh(geom, mat);
             
-            // Procedurálna matematika: Náhodné rozmiestnenie tykadiel po povrchu bunky
             const phi = Math.acos((Math.random() * 2) - 1);
             const theta = Math.random() * Math.PI * 2;
-            
-            // Namierime ich von z jadra
             spike.position.setFromSphericalCoords(1.0, phi, theta);
             spike.lookAt(0,0,0);
             spike.rotation.x -= Math.PI / 2; 
 
-            // Uložíme si unikátny seed pre "organické vlnenie" tohto konkrétneho tykadla
             spike.userData = { 
-                baseRotX: spike.rotation.x, 
-                baseRotZ: spike.rotation.z,
-                waveSpeed: 0.05 + Math.random() * 0.05,
-                waveOffset: Math.random() * Math.PI * 2
+                baseRotX: spike.rotation.x, baseRotZ: spike.rotation.z,
+                waveSpeed: 0.05 + Math.random() * 0.05, waveOffset: Math.random() * Math.PI * 2
             };
 
             this.core.add(spike);
             this.appendages.push(spike);
         }
         
-        // Ak nás predátor zraní a klesne nám biomasa, strácame tykadlá (regresia)
         if (this.appendages.length > targetAppendages) {
             const lostSpike = this.appendages.pop();
             this.core.remove(lostSpike);
@@ -98,7 +89,11 @@ window.Player = {
     update() {
         if (!this.mesh) return;
 
-        // Kontrola, či sa nemáme fyzicky vyvinúť
+        // ANIMÁCIA SHADERU (Tekutý efekt)
+        if (this.coreMat) {
+            this.coreMat.uniforms.time.value = Date.now() * 0.001;
+        }
+
         this.evolveShape();
 
         if (window.Entities) {
@@ -111,11 +106,9 @@ window.Player = {
         this.posY += this.velocityY;
         this.mesh.position.set(this.posX, this.posY, 0);
 
-        // Rotácia jadra
         this.core.rotation.y += 0.01;
         this.core.rotation.x += 0.005;
 
-        // 🦑 Organické vlnenie tykadiel vo vode (ASMR efekt)
         const time = Date.now();
         for (let spike of this.appendages) {
             spike.rotation.x = spike.userData.baseRotX + Math.sin(time * spike.userData.waveSpeed + spike.userData.waveOffset) * 0.2;
